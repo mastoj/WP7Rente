393459ba74f3650ab5aa8ba77fa74eb810d8bf9d
Tomas Jansson <tomas.jansson@gmail.com>
2012-01-13 22:54:04 +0100

    Initial commit

---
+++ Rente/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+
+namespace Rente
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _amount = 100000;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string Amount
+        {
+            get
+            {
+                return Amount;
+            }
+            set
+            {
+                if (value != Amount)
+                {
+                    Amount = value;
+                    NotifyPropertyChanged("Amount");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
